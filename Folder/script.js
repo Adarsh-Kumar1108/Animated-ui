17fc1282ff6fd991d3ca54ea91bd7ce8958eed9e
Adarsh Kumar <adarsh8709392288@gmail.com>
2024-12-28 13:46:28 +0530

    first commit

---
+++ Folder/script.js
@@ -0,0 +1,12 @@
+
+const typeoforder = document.getElementById('Type');
+
+console.log(typeoforder.value);
+
+
+
+// typeoforder.addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     document.write(typeoforder.value)
+
+// })
